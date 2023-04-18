7edb19b13caebc31438ab52bfd61410eb6d49b14
JeiversonChristian <jeiversonc@gmail.com>
2023-04-18 15:27:19 -0300

    pipe finished - const | init | height | move | | draw | collide

--- SmartBird.py
+++ SmartBird.py
@@ -21,9 +21,9 @@ import random
 # screen dimensions
 
 # screen width
-SW = 500
+SCREEN_WIDTH = 500
 # screen height
-SH = 800
+SCREEN_HEIGHT = 800
 
 # images
 
@@ -139,7 +139,7 @@ class Bird:
                 if self.angle > -90:
                     self.angle -= self.ROTATION_VELOCITY
 
-    # draw bird in the screen
+    # draw Bird in the screen
     def draw(self, screen):
         
         # define which Bird image that is going to be used
@@ -179,10 +179,85 @@ class Bird:
         pygame.mask.from_surface(self.image)
 
     # END Bird
-    
+
 
 class Pipe:
-    pass
+    
+    # defining Pipe constants
+
+    # distance between the pipes (one above, one below)
+    DISTANCE =  200
+
+    # the Pipes are going to move in the x axis
+    VELOCITY = 5
+
+    # the Pipes needs the x point to be drawn, but the "y" is randon
+    def __init__(self, x):
+        
+        self.x = x
+
+        # reference point to draw the pipes -- "y"
+        self.height = 0 
+
+        # positions of the "bases" of the pipes
+        self.pos_top = 0
+        self.pos_bot= 0
+
+        # images - they are CONSTANT
+        # the pipe image is flipped in the y axis ---------- (x,y)
+        self.TOP_PIPE_IMG = pygame.transform.flip(PIPE_IMG, False, True)
+        self.BOT_PIPE_IMG = PIPE_IMG
+
+        # parameter
+        # the bird has already passed by?
+        self.passed = False
+
+        # function call
+        self.define_height()
+
+    def define_height(self):
+
+        # (50,450) --- to not to be impossible to the bird to pass by
+        self.height = random.randrange(50, 450)
+        self.pos_top = self.height - self.TOP_PIPE_IMG.get_height()
+        self.pos_bot = self.height + self.DISTANCE
+
+    def move(self):
+
+        # x axis is negative to the left
+        self.x -= self.VELOCITY
+
+     # draw Pipe in the screen
+    def draw(self, screen):
+        screen.blit(self.TOP_PIPE_IMG, (self.x, self.pos_top))
+        screen.blit(self.BOT_PIPE_IMG, (self.x, self.pos_bot))
+
+    # verify if the Pipe collides with the Bird
+    def collide(self, bird):
+
+        # getting the masks of the objects
+        bird_mask = bird.get_mask()
+        top_pipe_mask = pygame.mask.from_surface(self.TOP_PIPE_IMG)
+        bot_pipe_mask = pygame.mask.from_surface(self.BOT_PIPE_IMG)
+
+        # calculating the distances between the objects
+        # round() is to round the number to an integer, because the bird can have a float one
+        distance_top_pipe = (self.x - round(bird.x), self.pos_top - round(bird.y))
+        distance_bot_pipe = (self.x - round(bird.x), self.pos_bot - round(bird.y))
+
+        # verify the collision with mask
+        # overlap() are there two equal pixel overlapping each other?
+        top_collided = bird_mask.overlap(top_pipe_mask, distance_top_pipe) # boolean value
+        bot_collided = bird_mask.overlap(bot_pipe_mask, distance_bot_pipe) # boolean value
+
+        # There are collision
+        if top_collided == True or bot_collided == True:
+            return True
+        else:
+            return False
+        
+        # END Pipe
+
 
 class Base:
     pass
